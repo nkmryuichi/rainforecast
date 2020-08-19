323f9b6f07ecb79ca663389863724b1cac5b9444
nakamurayuichi <affliate.yuichi@gmail.com>
2020-08-19 20:19:22 +0900

    Update bad request

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -10,7 +10,7 @@ class LinebotController < ApplicationController
     body = request.body.read
     signature = request.env['HTTP_X_LINE_SIGNATURE']
     unless client.validate_signature(body, signature)
-      return head :bad_request
+      error 400 do 'Bad Request' end
     end
     events = client.parse_events_from(body)
     events.each { |event|
